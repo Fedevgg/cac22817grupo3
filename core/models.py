80f124cfa5393c32ba1784eeba2c515598b0ce7d
Fedevgg <federico.viggiani@gmail.com>
2022-10-18 08:03:52 -0300

    Agreda la app products y empezado en models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+ 
+    class Meta:
+        verbose_name = ("Product")
+        verbose_name_plural = ("Products")
+
+    def __str__(self):
+        return self.name
+
