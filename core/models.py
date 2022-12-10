a16dcea7d74f35d058af03dd75f59ada4f023b41
Mario Federico Viggiani <federico.viggiani@gmail.com>
2022-12-10 11:48:04 -0300

    Contacts guarda el msj en la db

--- core/models.py
+++ core/models.py
@@ -5,9 +5,15 @@ from django.db import models
 class Contact(models.Model):
     nombre = models.CharField(max_length=50)
     apellido = models.CharField(max_length=50)
-    edad = models.SmallIntegerField()
+#    edad = models.SmallIntegerField()
     correo = models.EmailField(max_length=254)
     consulta = models.TextField()
+
+    class Meta:
+        db_table= "consultas"
+        verbose_name="consulta"
+        verbose_name_plural="consultas"
+        ordering=["-id"]
 
     def __str__(self):
         return self.nombre
