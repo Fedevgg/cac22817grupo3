35b440830a7de24e6f77535d707e680b2f1798ed
Mario Federico Viggiani <federico.viggiani@gmail.com>
2022-12-10 15:13:06 -0300

    Agregado el login de Luciana, faltan el carrito y estilos

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path, include
 from . import views
-
+from django.contrib.auth import views as auth_views
 
 core_patterns = [
     path('', views.index, name='home'),
     path('contact/', views.contact, name='contact'),
-    path('cart/', include('cart.urls'), name='cart')
+    path('cart/', include('cart.urls'), name='cart'),
+    path('registrarse/', views.core_registrarse, name='registrarse'),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('login/', views.core_login, name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='core/index.html'), name='logout'),
+    path('compras/', views.compras, name='compras')
 ]
